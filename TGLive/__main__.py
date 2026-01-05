1d951fa71b6a551f8ce2e09a320d3a1142dee38f
Sarbeswar <inc.arctix@gmail.com>
2026-01-05 19:59:59 +0530

    fix

--- TGLive/__main__.py
+++ TGLive/__main__.py
@@ -74,7 +74,7 @@ async def main():
             getattr(client.me, "username", None),
         )
 
-        store = JsonPlaylistStore()
+        # store = JsonPlaylistStore()
         store = PostgresPlaylistStore(Telegram.POSTGRES_URL)
 
         manager = VideoPlaylistManager(
